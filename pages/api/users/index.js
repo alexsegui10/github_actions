bedce706573d44429a43854325d511f68e9efcdb
alexsegui10 <alexsegui10@gmail.com>
2025-10-22 17:27:51 +0200

    linter

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -8,10 +8,12 @@ function handler(req, res) {
       return getUsers();
     case "POST0":
       return createUser();
-    default:
-        return res.status(405).end(`Method ${req.method} Not Allowed`);
+
     case "DELETE":
       return deleteAllUsers();
+      
+    default:
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
 
   }
 
